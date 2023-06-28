f24adb182897bca563b1d54eb1615e27481874ad
yaswankar <yaswankar1998@gmail.com>
2023-06-28 18:53:11 +0530

    first commit

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Plugin component</div>
+</template>
+<script>
+    export default {
+        name: 'component-a',
+        data() {
+            return {
+                a: 1,
+                b: 2
+            };
+        }
+    };
+</script>
