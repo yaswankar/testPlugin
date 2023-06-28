72664120f57d56edff94ca7e0be9de714ec6ab2a
yaswankar <yaswankar1998@gmail.com>
2023-06-28 19:42:01 +0530

    Updated main.js content

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+import componentA from './components/ComponentA';
+const MyPlugin = {
+    install(Vue, rootStore) {
+        // Vue is a Vue Constructor, not an Vue instance. This plugin requires passing the main app Vue instance as options parameter.
+// Vue.component need to be called BEFORE new Vue() in main.js...
+        Vue.component(componentA.name, componentA);
+    }
+}
+export default MyPlugin;
+export {componentA}
